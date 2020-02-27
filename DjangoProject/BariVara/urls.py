900f7685d8a3849e6aca509937a01f5135fc8bad
Md_Mashrur_Sadat <mashrur.sadat@northsouth.edu>
2020-02-27 22:00:23 +0600

    changed frontend of some pages

--- DjangoProject/BariVara/urls.py
+++ DjangoProject/BariVara/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     
     path('', AdvertisementListView.as_view(), name='HomePage'),
     path('advertisements/<int:pk>/',AdvertisementDetailsView.as_view() , name='advertisement_details'),
-    path('/create_advertisements',AdvertisementCreateView.as_view(), name='create_advertisements'),
+    path('create_advertisements',AdvertisementCreateView.as_view(), name='create_advertisements'),
     path('your_advertisements',views.your_advertisements, name='your_advertisements'),
     path('advertisements/<int:pk>/update',AdvertisementUpdateView.as_view() , name='advertisement_update'),
     path('advertisements/<int:pk>/delete',AdvertisementDeleteView.as_view() , name='advertisement_delete'),
